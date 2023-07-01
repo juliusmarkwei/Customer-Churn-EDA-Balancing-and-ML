037d09cf33e549882c58f5bf8272c3f99d519674
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-07-01 01:13:23 +0000

    Revert "configured packages"
    This reverts commit 4fce18062e2017e7456f2befdefbff8e591257d1.

--- venv/lib/python3.10/site-packages/yellowbrick/contrib/__init__.py
+++
@@ -1,7 +0,0 @@
-# yellowbrick.contrib
-#
-# Contains a variety of extra tools and experimental visualizers outside of
-# core support or still in development.
-#
-#
-# ID: __init__.py [a60bc41] nathan.danielsen@gmail.com $
