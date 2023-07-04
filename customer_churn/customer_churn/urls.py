18edfa1f1c44de6f7ee6f01b0687dc7f29acebe8
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-07-04 03:28:28 +0000

    add template

--- customer_churn/customer_churn/urls.py
+++ customer_churn/customer_churn/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Home Page')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('home.urls'))
 ]
