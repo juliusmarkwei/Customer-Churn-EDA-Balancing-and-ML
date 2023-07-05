7aa94c3c2d745a98611d393032f12e4adb5e25ea
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-07-05 04:29:45 +0000

    creating a main template

--- customer_churn/home/views.py
+++ customer_churn/home/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home/dashboard.html')
 
