ac77118737b5d3a5fdad18b95b892ce5ab7be28e
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-09-16 11:10:45 +0000

    import attrib

--- app.py
+++ app.py
@@ -1,34 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
+# import main
+from main import attrib
 
-
-components.html(
-    """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <style>
-                
-            </style>
-        </head>
-        <body>
-            <!-- Navbar HTML -->
-            <div class="navbar">
-                <a href="#" class="logo">My Website</a>
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Services</a>
-                <a href="#">Portfolio</a>
-                <a href="#">Contact</a>
-            </div>
-
-            <!-- Content goes here -->
-
-        </body>
-        </html>
-
-    """,
-    
-)
+print(attrib)
