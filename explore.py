a9c5636c4f3de03d51124e8873306f91e3af75c5
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-09-20 17:17:39 +0000

    added card_type vs exited chart

--- explore.py
+++ explore.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 # importing dataframe for some exploration purposes
@@ -12,20 +11,6 @@ def show_explore_page():
     st.write("""### Exploration on the Customer Churn Database""")
 
     global df
-    geo_counts = df["Geography"].value_counts()
-    fig = go.Figure(data=[go.Bar(x=geo_counts.index, y=geo_counts.values)])
-
-    # Set plot title and axis labels
-    fig.update_layout(
-        title="Geography Distribution of the Train/Test Data",
-        xaxis_title="Geography",
-        yaxis_title="Count",
-    )
-
-    # Display the plot using Streamlit
-    st.plotly_chart(fig)
-
-    # second chart
     # Group the data by "Geography" and calculate the Exited counts
     geo_Exited_counts = (
         df.groupby(["Geography", "Exited"]).size().unstack(fill_value=0).reset_index()
@@ -59,3 +44,44 @@ def show_explore_page():
 
     # Display the plot using Streamlit
     st.plotly_chart(fig)
+
+    # Group the data by "Card Type" and calculate the exit counts
+    credit_card_exit_counts = (
+        df.groupby(["Card Type", "Exited"]).size().unstack(fill_value=0).reset_index()
+    )
+
+    # Create a grouped bar chart using Plotly with cool colors
+    cool_colors = [
+        "#1f77b4",
+        "#2ca02c",
+    ]  # Blue and green colors for "Exited" and "Not Exited"
+
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                x=credit_card_exit_counts["Card Type"],
+                y=credit_card_exit_counts[1],
+                name="Exited",
+                marker_color=cool_colors[0],
+            ),
+            go.Bar(
+                x=credit_card_exit_counts["Card Type"],
+                y=credit_card_exit_counts[0],
+                name="Not Exited",
+                marker_color=cool_colors[1],
+            ),
+        ]
+    )
+
+    # Set plot title and axis labels
+    fig.update_layout(
+        title="Card Type Exit Counts",
+        xaxis_title="Card Type",
+        yaxis_title="Count",
+        barmode="group",  # To create grouped bars
+    )
+
+    # Display the plot using Streamlit
+    st.plotly_chart(fig)
+    
+    
