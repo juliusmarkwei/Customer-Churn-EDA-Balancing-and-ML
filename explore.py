d373cc875e2a59ea2cc2b18cc430df2ca0f2e2df
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-09-20 17:31:12 +0000

    changed chart to stacked

--- explore.py
+++ explore.py
@@ -23,13 +23,13 @@ def show_explore_page():
                 x=geo_Exited_counts["Geography"],
                 y=geo_Exited_counts[1],
                 name="Exiteded",
-                marker_color="yellow",
+                marker_color="#bf9a40",
             ),
             go.Bar(
                 x=geo_Exited_counts["Geography"],
                 y=geo_Exited_counts[0],
                 name="Not Exiteded",
-                marker_color="orange",
+                marker_color="#c03f77",
             ),
         ]
     )
@@ -46,39 +46,33 @@ def show_explore_page():
     st.plotly_chart(fig)
 
     # Group the data by "Card Type" and calculate the exit counts
-    credit_card_exit_counts = (
-        df.groupby(["Card Type", "Exited"]).size().unstack(fill_value=0).reset_index()
-    )
+    credit_card_exit_counts = df.groupby(["Card Type", "Exited"]).size().unstack(fill_value=0).reset_index()
 
-    # Create a grouped bar chart using Plotly with cool colors
-    cool_colors = [
-        "#1f77b4",
-        "#2ca02c",
-    ]  # Blue and green colors for "Exited" and "Not Exited"
+    # Create a stacked bar chart using Plotly with cool colors
+    cool_colors = ['#5EA35C', '#8045ba']  # Blue and green colors for "Exited" and "Not Exited"
 
-    fig = go.Figure(
-        data=[
-            go.Bar(
-                x=credit_card_exit_counts["Card Type"],
-                y=credit_card_exit_counts[1],
-                name="Exited",
-                marker_color=cool_colors[0],
-            ),
-            go.Bar(
-                x=credit_card_exit_counts["Card Type"],
-                y=credit_card_exit_counts[0],
-                name="Not Exited",
-                marker_color=cool_colors[1],
-            ),
-        ]
-    )
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=credit_card_exit_counts["Card Type"],
+        y=credit_card_exit_counts[1],
+        name='Exited',
+        marker_color=cool_colors[0]
+    ))
+
+    fig.add_trace(go.Bar(
+        x=credit_card_exit_counts["Card Type"],
+        y=credit_card_exit_counts[0],
+        name='Not Exited',
+        marker_color=cool_colors[1]
+    ))
 
     # Set plot title and axis labels
     fig.update_layout(
-        title="Card Type Exit Counts",
+        title="Card Type Exit Counts (Stacked)",
         xaxis_title="Card Type",
         yaxis_title="Count",
-        barmode="group",  # To create grouped bars
+        barmode='stack'  # To create stacked bars
     )
 
     # Display the plot using Streamlit
