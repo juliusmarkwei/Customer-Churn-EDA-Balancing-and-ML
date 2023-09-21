40625f2f0fc58c776fb17d146b46c59376e0c41b
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-09-21 00:17:19 +0000

    added img func

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from main import predict_page, tab
 from explore import show_explore_page
 
 
-
 def explore():
     selection = st.sidebar.selectbox(
         ":Yellow[What do you want to know about my project?]",
@@ -12,15 +11,20 @@ def explore():
     return selection
 
 
-if __name__ == "__main__":
-    ok = explore()
-    st.sidebar.image(
+def img():
+    image_object = st.sidebar.image(
         "https://cdn.analyticsvidhya.com/wp-content/uploads/2020/05/Churn-Prediction-scaled.jpg"
     )
+    return image_object
+
+
+if __name__ == "__main__":
+    ok = explore()
+    img()
 
     if ok == "Make predictions":
         predict_page()
     else:
         show_explore_page()
 
-tab()
+    tab()
